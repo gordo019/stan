da7d138a86ca1fde8b630249d332352b68d8b0ab
Gordon <28gordon28@mail.com>
2017-12-29 14:31:57 -0700

    add about template

--- single.php
+++ single.php
@@ -4,34 +4,35 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
- * @package StrapPress
+ * @package StanleyWP
  */
-
 get_header(); ?>
 
-	<div class="container">
-		<div class="row">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
+	
+		<div id="primary" class="content-area-full">
+			<main id="main" class="site-main" role="main">
 
-				<?php
-				while ( have_posts() ) : the_post();
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', get_post_format() ); ?>
 
-					get_template_part( 'template-parts/content', get_post_format() );
+				<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-md-8">
+								<?php the_post_navigation();
+								// If comments are open or we have at least one comment, load up the comment template.
+								if ( comments_open() || get_comments_number() ) :
+									comments_template();
+								endif; ?>
 
-					the_post_navigation();
+							</div><!--  .col-md-8 -->
+						</div><!--  .row -->
+					</div><!--  .container -->		
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+			<?php endwhile; // End of the loop. ?>
 
-				endwhile; // End of the loop.
-				?>
-
-				</main><!-- #main -->
-			</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
